3d77303954f86f055b4147d5f3d1240836c06b60
Reedus0 <lazsasha2006@gmail.com>
2025-04-12 19:11:27 +0300

    changed count

--- intel.py
+++ intel.py
@@ -52,7 +52,7 @@ def download_sample(hash):
 def collect_samples():
     raw_samples = []
     collectors = [
-        YarifyCollector(os.environ["ABUSE_API_KEY"]),
+        YarifyCollector(os.environ["ABUSE_API_KEY"], 50),
         AbuseCollector(os.environ["ABUSE_API_KEY"]),
         HybridAnalysisCollector(os.environ["HYBRID_ANALYSIS_API_KEY"])
     ]
